b73ec5cdee3662e7c97ff83e4f64b2e9c4bb9100
Luzors <jornvanbommel@gmail.com>
2023-11-16 14:42:10 +0100

    I have footer

---
+++ libs/sportify-basics/ui/src/lib/footer/footer.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sportify-nx-footer',
+    templateUrl: './footer.html',
+    styles: []
+})
+export class FooterComponent{}
--- libs/sportify-basics/ui/src/lib/ui.module.ts
+++ libs/sportify-basics/ui/src/lib/ui.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './navbar/navbar';
+import { FooterComponent } from './footer/footer';
 
 
 @NgModule({
   imports: [CommonModule], //RouterLink
-  declarations: [NavBarComponent],
-  exports: [NavBarComponent]
+  declarations: [NavBarComponent, FooterComponent],
+  exports: [NavBarComponent, FooterComponent]
 })
 export class UiModule {}
