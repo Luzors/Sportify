afb5c712999b8ffa49991f35cd570e92b8815f46
Luzors <jornvanbommel@gmail.com>
2023-11-26 23:16:05 +0100

    pls data-api work

--- libs/shared/util-env/src/lib/environment.dev.ts
+++ libs/shared/util-env/src/lib/environment.dev.ts
@@ -2,6 +2,6 @@ import { IEnvironment } from "./environment.interface";
 
 export const environment:IEnvironment = {
     production: false,
-    dataApiUrl: 'http://localhost:3000',
+    dataApiUrl: 'http://localhost:8080',
     databaseUrl: 'mongodb://127.0.0.1:27017/sportifymdb'
 }
